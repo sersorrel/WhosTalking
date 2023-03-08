58788f554c776376a61c8e65fe4c98bbc58cb4d0
ash <ash@sorrel.sh>
2023-03-08 00:05:37 +0000

    Show indicators (only when not in a party, currently)

--- WhosTalking/Plugin.cs
+++ WhosTalking/Plugin.cs
@@ -1,8 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.Numerics;
+using Dalamud.Game.ClientState.Party;
+using Dalamud.Game.Gui;
+using Dalamud.Interface;
 using Dalamud.Interface.Windowing;
 using Dalamud.IoC;
 using Dalamud.Plugin;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using ImGuiNET;
 using JetBrains.Annotations;
 using WhosTalking.Windows;
 
@@ -10,14 +16,19 @@ namespace WhosTalking;
 
 [PublicAPI]
 public sealed class Plugin: IDalamudPlugin {
+    private static Vector2 IndicatorOffset = new(-15, 0);
     internal DiscordConnection Connection;
     private Stack<Action> disposeActions = new();
     public WindowSystem WindowSystem = new("WhosTalking");
 
     public Plugin(
-        [RequiredVersion("1.0")] DalamudPluginInterface pluginInterface
+        [RequiredVersion("1.0")] DalamudPluginInterface pluginInterface,
+        [RequiredVersion("1.0")] GameGui gameGui,
+        [RequiredVersion("1.0")] PartyList partyList
     ) {
         this.PluginInterface = pluginInterface;
+        this.GameGui = gameGui;
+        this.PartyList = partyList;
 
         this.Configuration = this.PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
         this.Configuration.Initialize(this.PluginInterface);
@@ -43,6 +54,8 @@ public sealed class Plugin: IDalamudPlugin {
     }
 
     internal DalamudPluginInterface PluginInterface { get; init; }
+    internal GameGui GameGui { get; init; }
+    internal PartyList PartyList { get; init; }
     public Configuration Configuration { get; init; }
 
     internal ConfigWindow ConfigWindow { get; init; }
@@ -55,8 +68,99 @@ public sealed class Plugin: IDalamudPlugin {
         }
     }
 
+    private User? XivToDiscord(string name, string? world) {
+        return null;
+    }
+
     private void Draw() {
         this.WindowSystem.Draw();
+        this.DrawOverlay();
+    }
+
+    private unsafe void DrawOverlay() {
+        ImGuiHelpers.ForceNextWindowMainViewport();
+        ImGui.SetNextWindowPos(ImGui.GetMainViewport().Pos);
+        ImGui.SetNextWindowSize(ImGui.GetMainViewport().Size);
+        if (ImGui.Begin(
+                "##WhosTalkingOverlay",
+                ImGuiWindowFlags.NoDecoration
+                | ImGuiWindowFlags.NoSavedSettings
+                | ImGuiWindowFlags.NoMove
+                | ImGuiWindowFlags.NoMouseInputs
+                | ImGuiWindowFlags.NoFocusOnAppearing
+                | ImGuiWindowFlags.NoBackground
+                | ImGuiWindowFlags.NoNav
+            )) {
+            try {
+                ImGui.PushClipRect(
+                    ImGui.GetMainViewport().Pos,
+                    ImGui.GetMainViewport().Pos + ImGui.GetMainViewport().Size,
+                    false
+                );
+                var drawList = ImGui.GetWindowDrawList();
+                var partyAddon = (AtkUnitBase*)this.GameGui.GetAddonByName("_PartyList");
+                var shouldDrawParty = partyAddon != (AtkUnitBase*)nint.Zero && partyAddon->IsVisible;
+                if (shouldDrawParty) {
+                    if (this.PartyList.Length == 0) {
+                        var user = this.Connection.Self;
+                        DrawIndicator(drawList, partyAddon, 0, user);
+                    } else {
+                        for (var i = 0; i < this.PartyList.Length; i++) {
+                            var partyMember = this.PartyList[i]!;
+                            // TODO: work out what to do with partyMember.World
+                            var user = this.XivToDiscord(partyMember.Name.TextValue, null);
+                            DrawIndicator(drawList, partyAddon, i, user);
+                        }
+                    }
+                }
+            } finally {
+                ImGui.PopClipRect();
+                ImGui.End();
+            }
+        }
+    }
+
+    private static unsafe void DrawIndicator(ImDrawListPtr drawList, AtkUnitBase* partyAddon, int idx, User? user) {
+        var nodePtr = (AtkComponentNode*)partyAddon->UldManager.NodeList[22 - idx];
+        var colNode = nodePtr->Component->UldManager.NodeList[17]; // the image node for the job icon
+        var indicatorStart = GetNodePosition(colNode) + IndicatorOffset;
+        var indicatorSize = new Vector2(5, colNode->Height) * partyAddon->Scale;
+        var indicatorMin = indicatorStart + ImGui.GetMainViewport().Pos;
+        var indicatorMax = indicatorStart + indicatorSize + ImGui.GetMainViewport().Pos;
+        drawList.AddRectFilled(indicatorMin, indicatorMax, GetColour(user));
+    }
+
+    private static unsafe Vector2 GetNodePosition(AtkResNode* node) {
+        var pos = new Vector2(node->X, node->Y);
+        var par = node->ParentNode;
+        while (par != null) {
+            pos *= new Vector2(par->ScaleX, par->ScaleY);
+            pos += new Vector2(par->X, par->Y);
+            par = par->ParentNode;
+        }
+
+        return pos;
+    }
+
+    private static uint GetColour(User? user) {
+        // colours are ABGR
+        if (user == null) {
+            return 0xFF00FFFF; // yellow
+        }
+
+        if (user.Speaking.GetValueOrDefault()) {
+            return 0xFF00FF00; // green
+        }
+
+        if (user.Deafened.GetValueOrDefault()) {
+            return 0xFF0000FF; // red
+        }
+
+        if (user.Muted.GetValueOrDefault()) {
+            return 0xFF808000; // blue
+        }
+
+        return 0;
     }
 
     public void OpenConfigUi() {
--- WhosTalking/Windows/MainWindow.cs
+++ WhosTalking/Windows/MainWindow.cs
@@ -1,5 +1,8 @@
 using System;
+using Dalamud.Interface;
+using Dalamud.Interface.Components;
 using Dalamud.Interface.Windowing;
+using Dalamud.Utility;
 using ImGuiNET;
 
 namespace WhosTalking.Windows;
@@ -18,15 +21,28 @@ public sealed class MainWindow: Window, IDisposable {
 
     public override void Draw() {
         ImGui.TextUnformatted($"Current channel: {this.plugin.Connection.Channel?.Channel}");
-        ImGui.Text("Users:");
+
+        ImGui.Text($"Party (size {this.plugin.PartyList.Length}):");
+        foreach (var partyMember in this.plugin.PartyList) {
+            ImGui.TextUnformatted($"{partyMember.Name.TextValue}");
+        }
+
+        ImGui.Text("Discord users:");
         foreach (var user in this.plugin.Connection.AllUsers.Values) {
+            var muted = user.Muted.GetValueOrDefault();
+            var deafened = user.Deafened.GetValueOrDefault();
+            var speaking = user.Speaking.GetValueOrDefault();
+            var displayName = user.DisplayName.IsNullOrEmpty() ? user.Username : user.DisplayName;
             ImGui.TextUnformatted(
-                $"{user.DisplayName} ({user.Username}#{user.Discriminator}): {(user.Muted.GetValueOrDefault() ? "" : "un")}muted, {(user.Deafened.GetValueOrDefault() ? "" : "un")}deafened{(user.Speaking.GetValueOrDefault() ? ", speaking" : "")}"
+                $"{displayName}: {(muted ? "" : "un")}muted, {(deafened ? "" : "un")}deafened{(speaking ? ", speaking" : "")}"
             );
+            if (ImGui.IsItemHovered()) {
+                ImGui.SetTooltip($"{user.Username}#{user.Discriminator}");
+            }
         }
 
         ImGui.Separator();
-        if (ImGui.Button("Show Settings")) {
+        if (ImGuiComponents.IconButton(FontAwesomeIcon.Cog)) {
             this.plugin.OpenConfigUi();
         }
     }
